f207a5355a4e1d6277489b22e52c7735131e1d09
Sahal Zain <zean00zone@gmail.com>
2020-07-21 10:16:30 +0700

    update svc

--- svc/main.go
+++ svc/main.go
@@ -3,12 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/textproto"
 
 	"github.com/labstack/echo/v4"
 )
 
+const subjectHeader = "subject"
+const phoneHeader = "phone_number"
+
 func main() {
 	e := echo.New()
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			fmt.Println("DEBUG")
+			fmt.Println(c.Request().Header.Get(subjectHeader))
+			fmt.Println(c.Request().Header.Get(phoneHeader))
+			//fmt.Println(c.Request().Header.Get(textproto.CanonicalMIMEHeaderKey(subjectHeader)))
+			if c.Request().Header.Get(subjectHeader) == "" {
+				c.Request().Header.Set(subjectHeader, c.Request().Header.Get(textproto.CanonicalMIMEHeaderKey(subjectHeader)))
+			}
+
+			if c.Request().Header.Get(phoneHeader) == "" {
+				c.Request().Header.Set(phoneHeader, c.Request().Header.Get(textproto.CanonicalMIMEHeaderKey(phoneHeader)))
+			}
+
+			return next(c)
+		}
+	})
 	e.GET("/head", func(c echo.Context) error {
 		for k := range c.Request().Header {
 			fmt.Println(k, ":", c.Request().Header.Get(k))
