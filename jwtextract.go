94470273201dffa11c6aa9c67673b62834b2e05f
Sahal Zain <zean00zone@gmail.com>
2020-10-03 10:07:03 +0700

    fix header getter

--- jwtextract.go
+++ jwtextract.go
@@ -115,10 +115,17 @@ func extractClaim(config *xtraConfig, r *proxy.Request) error {
 	if config == nil {
 		return errors.New("Empty config, skip")
 	}
-	token := r.Headers[authHeader][0]
+
+	token := ""
+	auth, ok := r.Headers[authHeader]
+	if ok {
+		token = auth[0]
+	}
+
 	if token == "" {
 		return errors.New("Token is empty, skip extracting ")
 	}
+
 	token = strings.TrimPrefix(token, "Bearer ")
 	//Just in case using lower case bearer
 	token = strings.TrimPrefix(token, "bearer ")
